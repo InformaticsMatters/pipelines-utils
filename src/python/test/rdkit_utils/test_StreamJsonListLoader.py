a58545da19725b125a14c0c10823eda4aaac6d4c
Alan Christie <alan.christie@matildapeak.com>
2018-02-05 10:43:10 +0000

    - Another StreamJsonListLoader test

--- src/python/test/rdkit_utils/test_StreamJsonListLoader.py
+++ src/python/test/rdkit_utils/test_StreamJsonListLoader.py
@@ -14,14 +14,14 @@ class StreamJsonListLoaderTestCase(unittest.TestCase):
         loader = StreamJsonListLoader.StreamJsonListLoader(test_file)
         # Load the JSON list (one at a time)
         entry = next(loader)
-        self.assertEquals('11', entry['a'])
-        self.assertEquals('12', entry['b'])
+        self.assertEqual('11', entry['a'])
+        self.assertEqual('12', entry['b'])
         entry = next(loader)
-        self.assertEquals('21', entry['a'])
-        self.assertEquals('22', entry['b'])
+        self.assertEqual('21', entry['a'])
+        self.assertEqual('22', entry['b'])
         entry = next(loader)
-        self.assertEquals('31', entry['a'])
-        self.assertEquals('32', entry['b'])
+        self.assertEqual('31', entry['a'])
+        self.assertEqual('32', entry['b'])
         got_end_of_data = False
         try:
             next(loader)
@@ -29,3 +29,15 @@ class StreamJsonListLoaderTestCase(unittest.TestCase):
             got_end_of_data = True
         self.assertTrue(got_end_of_data)
         loader.close()
+
+    def test_bad_1_file(self):
+        """Test loading of a simple JSON file (that's not a list)
+        """
+        test_file = os.path.join(DATA_DIR, 'StreamJsonListLoader.bad.1.json')
+        got_exception = False
+        try:
+            StreamJsonListLoader.StreamJsonListLoader(test_file)
+        except NotImplementedError as e:
+            self.assertTrue(str(e).startswith('Only JSON-streams of lists'))
+            got_exception = True
+        self.assertTrue(got_exception)
