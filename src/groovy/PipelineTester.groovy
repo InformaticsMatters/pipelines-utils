1bf475c3a271cea0f749f2c3c3632922b907747d
Alan Christie <alan.christie@matildapeak.com>
2018-10-26 15:24:51 +0100

    - Updated tester version

--- src/groovy/PipelineTester.groovy
+++ src/groovy/PipelineTester.groovy
@@ -33,7 +33,7 @@ import org.apache.commons.cli.Option
 
 // Version
 // Update with every change/release
-String version = '2.6.2'
+String version = '2.6.3'
 
 println "+------------------+"
 println "|  PipelineTester  | v$version"
